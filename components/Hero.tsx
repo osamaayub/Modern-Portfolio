113bb898a0e3fb069219a4c13b9df2d14a1fb02a
Osama Ayub <ayubosama416@gmail.com>
2024-08-28 22:43:51 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
        */}
       <div
         className='h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center'
+       absolute top-0 left-0 flex items-center justify-center white:bg-black-100'
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
