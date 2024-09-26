0f1c76483a4fcd03d7cccd81290444e61f249c49
Osama Ayub <ayubosama416@gmail.com>
2024-09-26 21:46:26 +0500

    Update TextGenerateEffect.tsx

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -36,7 +36,7 @@ export const TextGenerateEffect = ({
               key={word + idx}
               // change here if idx is greater than 3, change the text color to #CBACF9
               className={` ${
-                idx > 3 ? 'text-purple' : 'dark:text-white text-black'
+                idx > 3 ? 'text-purple' : 'dark:text-white text-black white:bg-black-100'
               } opacity-0`}
             >
               {word}{' '}
